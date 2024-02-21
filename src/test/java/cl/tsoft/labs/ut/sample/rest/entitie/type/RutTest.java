7bb22c0e6d472232dd1d0ce22cae4cccff2cc675
manoluky <46540583+manoluky@users.noreply.github.com>
2024-02-21 13:05:30 -0300

    adas

--- src/test/java/cl/tsoft/labs/ut/sample/rest/entitie/type/RutTest.java
+++ src/test/java/cl/tsoft/labs/ut/sample/rest/entitie/type/RutTest.java
@@ -1,5 +1,6 @@
 package cl.tsoft.labs.ut.sample.rest.entitie.type;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -8,57 +9,19 @@ import org.junit.jupiter.params.provider.CsvFileSource;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RutTest {
+
     @Test
-    public void test_create_rut_with_valid_number_and_digit() {
+    public void test_create_rut_with_valid_parameters() {
+        // Arrange
         long number = 12345678;
         char digit = 'K';
 
+        // Act
         Rut rut = new Rut(number, digit);
 
+        // Assert
         assertEquals(number, rut.getNumber());
         assertEquals(digit, rut.getDigit());
     }
-    @Test
-    public void test_is_valid_with_valid_rut() {
-        long number = 12345678;
-        char digit = 'K';
-
-        Rut rut = new Rut(number, digit);
-
-        assertTrue(rut.isValid());
-    }
-    @Test
-    public void test_calculate_digit_with_valid_number() {
-        long number = 12345678;
-        char expectedDigit = 'K';
-
-        char calculatedDigit = Rut.calculateDigit(number);
-
-
-        assertEquals(expectedDigit, calculatedDigit);
-    }
-
-    @Test
-    public void test_value_of_with_valid_string_representation() {
-        String rutText = "12345678-K";
-        long expectedNumber = 12345678;
-        char expectedDigit = 'K';
-
-        Rut rut = Rut.valueOf(rutText);
-
-        assertEquals(expectedNumber, rut.getNumber());
-        assertEquals(expectedDigit, rut.getDigit());
-    }
-    @Test
-    public void test_to_string_with_valid_rut() {
-        long number = 12345678;
-        char digit = 'K';
-        String expectedString = "12345678-K";
-
-        Rut rut = new Rut(number, digit);
-
-        assertEquals(expectedString, rut.toString());
-    }
-
 
 }
