e9be5eabaabc479aa15eba8702c843c1be74eb48
manoluky <46540583+manoluky@users.noreply.github.com>
2024-02-21 12:55:28 -0300

    adas

--- src/test/java/cl/tsoft/labs/ut/sample/rest/entitie/type/RutTest.java
+++ src/test/java/cl/tsoft/labs/ut/sample/rest/entitie/type/RutTest.java
@@ -8,164 +8,57 @@ import org.junit.jupiter.params.provider.CsvFileSource;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RutTest {
-
     @Test
-    @DisplayName("create empty rut")
-    void create_empty_rut() {
-        // Given
-        Rut rut;
+    public void test_create_rut_with_valid_number_and_digit() {
+        long number = 12345678;
+        char digit = 'K';
 
-        // When
-        rut = new Rut();
-
-        // Then
-        assertNotNull(rut);
-    }
-
-    @ParameterizedTest
-    @CsvFileSource(resources = "/valid-ruts.csv")
-    @DisplayName("should return true with valid rut")
-    void should_return_true_with_valid_rut(long number, char digit) {
-        // Given
         Rut rut = new Rut(number, digit);
 
-        // When
-        boolean isValid = rut.isValid();
-
-        // Then
-        assertTrue(isValid);
+        assertEquals(number, rut.getNumber());
+        assertEquals(digit, rut.getDigit());
     }
+    @Test
+    public void test_is_valid_with_valid_rut() {
+        long number = 12345678;
+        char digit = 'K';
 
-    @ParameterizedTest
-    @CsvFileSource(resources = "/invalid-ruts.csv")
-    @DisplayName("should return false with invalid rut")
-    void should_return_false_with_invalid_rut(long number, char digit) {
-        // Given
         Rut rut = new Rut(number, digit);
 
-        // When
-        boolean isValid = rut.isValid();
-
-        // Then
-        assertFalse(isValid);
+        assertTrue(rut.isValid());
     }
+    @Test
+    public void test_calculate_digit_with_valid_number() {
+        long number = 12345678;
+        char expectedDigit = 'K';
 
-    @ParameterizedTest
-    @CsvFileSource(resources = "/valid-ruts.csv")
-    @DisplayName("should return true with valid rut - static")
-    void should_return_true_with_valid_rut_static(long number, char digit) {
-        // Given
-        // Son los parámetros de entrada
+        char calculatedDigit = Rut.calculateDigit(number);
 
-        // When
-        boolean isValidStatic = Rut.isValid(number, digit);
 
-        // Then
-        assertTrue(isValidStatic);
-    }
-
-    @ParameterizedTest
-    @CsvFileSource(resources = "/invalid-ruts.csv")
-    @DisplayName("should return false with invalid rut - static")
-    void should_return_false_with_invalid_rut_static(long number, char digit) {
-        // Given
-        // Son los parámetros de entrada
-
-        // When
-        boolean isValidStatic = Rut.isValid(number, digit);
-
-        // Then
-        assertFalse(isValidStatic);
+        assertEquals(expectedDigit, calculatedDigit);
     }
 
     @Test
-    @DisplayName("should successful convert rut from valid string")
-    void should_successful_convert_rut_from_valid_string() {
-        // Given
-        String rutText = "40262650-K";
+    public void test_value_of_with_valid_string_representation() {
+        String rutText = "12345678-K";
+        long expectedNumber = 12345678;
+        char expectedDigit = 'K';
 
-        // When
-        Rut rutObject = Rut.valueOf(rutText);
+        Rut rut = Rut.valueOf(rutText);
 
-        // Then
-        assertNotNull(rutObject);
+        assertEquals(expectedNumber, rut.getNumber());
+        assertEquals(expectedDigit, rut.getDigit());
     }
-
     @Test
-    @DisplayName("should return null when valueOf with null string")
-    void should_return_null_when_valueOf_with_null_string() {
-        // Given
-        String rutNull = null;
+    public void test_to_string_with_valid_rut() {
+        long number = 12345678;
+        char digit = 'K';
+        String expectedString = "12345678-K";
 
-        // When
-        Rut rut = Rut.valueOf(rutNull);
+        Rut rut = new Rut(number, digit);
 
-        // Then
-        assertNull(rut);
+        assertEquals(expectedString, rut.toString());
     }
 
-    @Test
-    @DisplayName("should return null when valueOf with empty string")
-    void should_return_null_when_valueOf_with_empty_string() {
-        // Given
-        String rutEmpty = "";
 
-        // When
-        Rut rut = Rut.valueOf(rutEmpty);
-
-        // Then
-        assertNull(rut);
-    }
-
-    @Test
-    @DisplayName("should throw exception when valueOf with invalid format - with points")
-    void should_throw_exception_when_valueOf_with_invalid_format() {
-        // Given
-        String rutInvalid = "12.345.678-0";
-
-        // When
-        Exception exception = assertThrowsExactly(IllegalArgumentException.class, () -> Rut.valueOf(rutInvalid));
-
-        // Then
-        assertEquals("rut has an invalid format", exception.getMessage());
-    }
-
-    @Test
-    @DisplayName("should throw exception when valueOf with invalid format - no dash")
-    void should_throw_exception_when_valueOf_with_invalid_format_no_dash() {
-        // Given
-        String rutInvalid = "123456780";
-
-        // When
-        Exception exception = assertThrowsExactly(IllegalArgumentException.class, () -> Rut.valueOf(rutInvalid));
-
-        // Then
-        assertEquals("rut has an invalid format", exception.getMessage());
-    }
-
-    @Test
-    @DisplayName("should throw exception when valueOf with invalid format - invalid char in digit")
-    void should_throw_exception_when_valueOf_with_invalid_format_invalid_char_digit() {
-        // Given
-        String rutInvalid = "12345678-L";
-
-        // When
-        Exception exception = assertThrowsExactly(IllegalArgumentException.class, () -> Rut.valueOf(rutInvalid));
-
-        // Then
-        assertEquals("rut has an invalid format", exception.getMessage());
-    }
-
-    @Test
-    @DisplayName("should throw exception when valueOf with invalid format - no digit")
-    void should_throw_exception_when_valueOf_with_invalid_format_no_digit() {
-        // Given
-        String rutInvalid = "12345678-";
-
-        // When
-        Exception exception = assertThrowsExactly(IllegalArgumentException.class, () -> Rut.valueOf(rutInvalid));
-
-        // Then
-        assertEquals("rut has an invalid format", exception.getMessage());
-    }
 }
