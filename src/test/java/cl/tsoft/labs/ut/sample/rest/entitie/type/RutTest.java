49f61fccc59f26e2f7736aa22e5c9d0dd06874b8
manoluky <46540583+manoluky@users.noreply.github.com>
2024-02-22 12:38:23 -0300

    Se test edgetest a RutTest 12:38 pm

--- src/test/java/cl/tsoft/labs/ut/sample/rest/entitie/type/RutTest.java
+++ src/test/java/cl/tsoft/labs/ut/sample/rest/entitie/type/RutTest.java
@@ -208,4 +208,9 @@ class RutTest {
         assertEquals(12345678, rut.getNumber());
         assertEquals('9', rut.getDigit());
     }
+    @Test
+    public void test_new_instance_with_negative_number() {
+        Rut rut = new Rut(-1, 'X');
+        assertEquals(0, rut.getNumber());
+    }
 }
